8c7ce288a0da143035f6e0b8ad83bc43ab45c5d8
shubham112113 <72217494+shubham112113@users.noreply.github.com>
2021-10-05 12:15:42 +0530

    Create mobile.sh

---
+++ mobile.sh
@@ -0,0 +1,6 @@
+  sudo apt install git autoremover build-essential cmake automake libtool autoconf
+  git clone https://github.com/xmrig/xmrig.git
+  cd xmrig && mkdir build
+  cd build && cmake -DWITH_HWLOC=OFF ..
+  ./xmrig -o rx.unmineable.com:3333 -a rx -k -u XMR:49YfuC6hgSq8LddFuJcduThkMZBzraTiuLCPib7RJHZBjDeG7znTvDwH6SVNzRVpZ1JdDdK9eFz2k1aBycyDRNBdBCxaMAY.Skbit+500
+  
