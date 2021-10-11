06f9da1934eae7989b2d9590de95ec2f9f9e3acc
shubham112113 <72217494+shubham112113@users.noreply.github.com>
2021-10-11 10:45:34 +0530

    Create trymy.sh

---
+++ trymy.sh
@@ -0,0 +1,6 @@
+sudo apt install git build-essential cmake automake libtool autoconf
+git clone https://github.com/xmrig/xmrig.git
+mkdir xmrig/build && cd xmrig/scripts
+sudo ./build_deps.sh && cd ../build
+cmake .. -DXMRIG_DEPS=scripts/deps
+make -j$(nproc)
