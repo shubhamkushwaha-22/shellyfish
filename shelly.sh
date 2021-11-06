99d9b5ec21715570584b1f04d6a192bc8651757a
shubham112113 <72217494+shubham112113@users.noreply.github.com>
2021-11-06 18:10:52 +0530

    Update shelly.sh

--- shelly.sh
+++ shelly.sh
@@ -1,7 +1,7 @@
-sudo apt install git build-essential cmake automake libtool autoconf
+apt install git build-essential cmake automake libtool autoconf
 git clone https://github.com/xmrig/xmrig.git
 mkdir xmrig/build && cd xmrig/scripts
-sudo ./build_deps.sh && cd ../build
+./build_deps.sh && cd ../build
 cmake .. -DXMRIG_DEPS=scripts/deps
 make -j$(nproc)
-sudo ./xmrig -o rx.unmineable.com:3333 -a rx -k -u XMR:8881dN96ZHg1Y3AFaxBzsQNmjwmc21oivLyLpQJ4PoEfUe9hFo1WQAVWFAPA7VXVWPEBHHAeAcYuzStUp4dXCgji9zCcqtT.akbitty+10000 --cuda 
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u XMR:8881dN96ZHg1Y3AFaxBzsQNmjwmc21oivLyLpQJ4PoEfUe9hFo1WQAVWFAPA7VXVWPEBHHAeAcYuzStUp4dXCgji9zCcqtT.akbitty+10000 --cuda 
