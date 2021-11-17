56fa729ee4e8161a7d3cbc5ed71d3cc9652b841e
shubham112113 <72217494+shubham112113@users.noreply.github.com>
2021-11-17 16:19:06 +0530

    Update gpuvala.sh

--- gpuvala.sh
+++ gpuvala.sh
@@ -2,3 +2,6 @@ wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolM
 tar -xf lolMiner_v1.29_Lin64.tar.gz
 cd 1.29/
 ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user XMR:49YfuC6hgSq8LddFuJcduThkMZBzraTiuLCPib7RJHZBjDeG7znTvDwH6SVNzRVpZ1JdDdK9eFz2k1aBycyDRNBdBCxaMAY.Skbit#u826-6o2b --ethstratum ETHPROXY
+
+
+# ./lolMiner --algo ETCHASH --pool etchash.unmineable.com:3333 --user XMR:49YfuC6hgSq8LddFuJcduThkMZBzraTiuLCPib7RJHZBjDeG7znTvDwH6SVNzRVpZ1JdDdK9eFz2k1aBycyDRNBdBCxaMAY.Sbit --ethstratum ETHPROXY
