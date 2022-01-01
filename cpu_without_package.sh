9575bec1ba2123e6e8aef2d37d4a014d7798e19a
shubham112113 <72217494+shubham112113@users.noreply.github.com>
2022-01-01 17:07:59 +0530

    Create cpu_without_package.sh

---
+++ cpu_without_package.sh
@@ -0,0 +1,8 @@
+sudo apt install wget git apt-utils
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.3/xmrig-6.15.3-linux-x64.tar.gz
+sudo apt install git build-essential cmake automake libtool autoconf
+tar -xf xmrig-6.15.3-linux-x64.tar.gz
+cd xmrig-6.15.3
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TAtHz7jiXQcBLYiL4oQxtPAX29LbAd8Tqy.akbit+100
+
+#./xmrig -o rx.unmineable.com:3333 -a rx -k -u XMR:49YfuC6hgSq8LddFuJcduThkMZBzraTiuLCPib7RJHZBjDeG7znTvDwH6SVNzRVpZ1JdDdK9eFz2k1aBycyDRNBdBCxaMAY.Skbit+5000
