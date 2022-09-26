aef7cbcf9f068f24ccc36302792797688315cc84
shubham112113 <72217494+shubham112113@users.noreply.github.com>
2022-09-26 15:03:44 +0530

    Create verusGPU.sh

---
+++ verusGPU.sh
@@ -0,0 +1,11 @@
+git clone --single-branch -b verus2.2gpu https://github.com/monkins1010/ccminer.git
+# cd ccminer
+# chmod +x build.sh
+# chmod +x configure.sh
+# chmod +x autogen.sh
+# ./build.sh
+
+
+
+
+./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RFG7WJYadVbox1QBGGkt1eCbA8g9TxMha4.hello -p x -d 0
