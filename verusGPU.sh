73a7609ab3cb81a6a68d73a56b8d5f35952d6173
shubham112113 <72217494+shubham112113@users.noreply.github.com>
2022-09-26 16:46:57 +0530

    Update verusGPU.sh

--- verusGPU.sh
+++ verusGPU.sh
@@ -1,9 +1,12 @@
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
+wget http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda_10.2.89_440.33.01_linux.run
+sudo sh cuda_10.2.89_440.33.01_linux.run
 git clone --single-branch -b verus2.2gpu https://github.com/monkins1010/ccminer.git
-# cd ccminer
-# chmod +x build.sh
-# chmod +x configure.sh
-# chmod +x autogen.sh
-# ./build.sh
+cd ccminer
+chmod +x build.sh
+chmod +x configure.sh
+chmod +x autogen.sh
+./build.sh
 
 
 
