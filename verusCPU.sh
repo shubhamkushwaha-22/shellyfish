fd8ecc4cf52b22e633c430668986bda688087e0c
shubham112113 <72217494+shubham112113@users.noreply.github.com>
2022-09-26 17:42:57 +0530

    Create verusCPU.sh

---
+++ verusCPU.sh
@@ -0,0 +1,1 @@
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar -xf hellminer_cpu_linux.tar.gz && ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RFG7WJYadVbox1QBGGkt1eCbA8g9TxMha4.cpu1 -p x --cpu 1
