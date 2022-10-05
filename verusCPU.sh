b2bb4b3cd2965544e1f06dccb65493185d6f2dbe
shubham112113 <72217494+shubham112113@users.noreply.github.com>
2022-10-05 16:48:36 +0530

    Update verusCPU.sh

--- verusCPU.sh
+++ verusCPU.sh
@@ -1,1 +1,10 @@
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar -xf hellminer_cpu_linux.tar.gz && ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RFG7WJYadVbox1QBGGkt1eCbA8g9TxMha4.cpu1 -p x --cpu 1
+#!/bin/sh
+sudo apt update -y
+sudo apt upgrade -y 
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar -xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RFG7WJYadVbox1QBGGkt1eCbA8g9TxMha4.cpu1 -p x --cpu 2
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
