57e120cf2a4c04e6494569df6ade362295b924f6
shubham112113 <72217494+shubham112113@users.noreply.github.com>
2022-10-18 18:04:27 +0530

    Add files via upload

---
+++ trail.py
@@ -0,0 +1,10 @@
+import time
+
+def trail():
+    print("Strating.......")
+    print("hello Github Actions")
+    print("Ending.........")
+
+
+while (time.time() - start_time) < max_time:
+    trail()
