ac92a9fb257c024c4ad6c09bfc1189fcd9f0a971
shubham112113 <72217494+shubham112113@users.noreply.github.com>
2022-10-18 18:08:31 +0530

    Update trail.py

--- trail.py
+++ trail.py
@@ -5,6 +5,8 @@ def trail():
     print("hello Github Actions")
     print("Ending.........")
 
+max_time = 36
+start_time = time.time()  # remember when we started
 
 while (time.time() - start_time) < max_time:
     trail()
